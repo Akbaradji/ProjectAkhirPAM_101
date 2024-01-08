d5a6e024ad14734d9d7385d8f35ba8216aca560a
Akbaradji <akbaradjiherlambang@gmail.com>
2024-01-08 20:51:25 +0700

    add fun Homepage

---
+++ app/src/main/java/com/example/projectakhirpam/ui/halaman/HalamanHome.kt
@@ -0,0 +1,13 @@
+package com.example.projectakhirpam.ui.halaman
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Homepage(
+    navigateToRent: () -> Unit,
+){
+    Column {
+
+    }
+}
