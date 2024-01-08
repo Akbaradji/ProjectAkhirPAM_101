752e4adba6c8614b76cea41b77234bb07b22c660
Akbaradji <akbaradjiherlambang@gmail.com>
2024-01-08 20:55:44 +0700

    Add centered OutlinedCard with custom border and padding"

--- app/src/main/java/com/example/projectakhirpam/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/projectakhirpam/ui/halaman/HalamanHome.kt
@@ -1,13 +1,38 @@
 package com.example.projectakhirpam.ui.halaman
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Homepage(
     navigateToRent: () -> Unit,
 ){
-    Column {
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        OutlinedCard (
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surface
+            ),
+            border = BorderStroke(1.dp, Color.Cyan),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(50.dp)
+                .align(Alignment.CenterHorizontally)
+        ){
 
+        }
     }
 }
