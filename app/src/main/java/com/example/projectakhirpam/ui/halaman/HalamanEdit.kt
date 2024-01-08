73d79e875fb32eaf7bb0e5b6d551c731b90a5d6e
Akbaradji <akbaradjiherlambang@gmail.com>
2024-01-08 23:27:16 +0700

    add Halaman Edit

---
+++ app/src/main/java/com/example/projectakhirpam/ui/halaman/HalamanEdit.kt
@@ -0,0 +1,46 @@
+package com.example.projectakhirpam.ui.halaman
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.ActivityNavigator
+import com.example.projectakhirpam.model.EditViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemEditScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold (
+        topBar = {
+            BeliTopAppBar(
+                title = stringResource(DestinasiEdit.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ){innerPadding ->
+        EntryPembeliBody(
+            uiStatePembeli = viewModel.pembeliUiState,
+            onPembeliValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
