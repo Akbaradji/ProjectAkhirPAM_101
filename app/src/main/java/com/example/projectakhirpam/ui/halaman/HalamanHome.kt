d40cf2d7c44d7b200f3e4ea1d063e3c263db69fd
Akbaradji <akbaradjiherlambang@gmail.com>
2024-01-08 21:00:15 +0700

    Menambahkan logo dengan penataan dan pembulatan sudut menggunakan Compose

--- app/src/main/java/com/example/projectakhirpam/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/projectakhirpam/ui/halaman/HalamanHome.kt
@@ -1,18 +1,23 @@
 package com.example.projectakhirpam.ui.halaman
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.projectakhirpam.R
 
 @Composable
 fun Homepage(
@@ -32,7 +37,19 @@ fun Homepage(
                 .padding(50.dp)
                 .align(Alignment.CenterHorizontally)
         ){
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "",
+                    Modifier.clip(
+                        RoundedCornerShape(10.dp)
+                    )
+                )
 
+            }
         }
     }
 }
