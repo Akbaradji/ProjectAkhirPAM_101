2af722fd227f582751954624f29280466435e2e8
Akbaradji <akbaradjiherlambang@gmail.com>
2024-01-08 20:31:44 +0700

    menambahkan databasepembeli

---
+++ app/src/main/java/com/example/projectakhirpam/data/Databasepembeli.kt
@@ -0,0 +1,26 @@
+package com.example.projectakhirpam.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [pembeli::class], version = 1, exportSchema = false)
+abstract class Databasepembeli: RoomDatabase(){
+    abstract fun pembeliDao(): pembeliDao
+
+    companion object{
+        @Volatile
+        private var Instance: Databasepembeli? = null
+
+        fun getDatabase(context: Context) : Databasepembeli {
+            return (Instance?: synchronized(this){
+                Room.databaseBuilder(context,
+                    Databasepembeli::class.java,
+                    "pembeli_database")
+                    .build()
+                    .also { Instance=it}
+            })
+        }
+    }
+}
